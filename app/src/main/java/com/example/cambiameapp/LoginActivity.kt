169e6bdc32a55e15b524670be0ba1f95feaa9565
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2023-06-30 00:06:47 -0400

    login db 2

--- app/src/main/java/com/example/cambiameapp/LoginActivity.kt
+++ app/src/main/java/com/example/cambiameapp/LoginActivity.kt
@@ -4,19 +4,32 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.cambiameapp.databinding.ActivityLoginBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailEt: EditText
+    private lateinit var passwordEt: EditText
+    private lateinit var loginBtn: Button
 
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        auth = FirebaseAuth.getInstance()
+        emailEt = binding.editTextUser
+        passwordEt = binding.editTextPassword
+        loginBtn = binding.buttonIngresar
 
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE) // will hide the title
         supportActionBar?.hide() // hide the title bar
@@ -28,14 +41,32 @@ class LoginActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
+
         } // enable full screen
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val loginButton: Button = findViewById(R.id.buttonIngresar)
         loginButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            var email: String = emailEt.text.toString()
+            var password: String = passwordEt.text.toString()
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                Toast.makeText(this@LoginActivity, "Por favor llenar todos los campos de texto", Toast.LENGTH_LONG).show()
+            } else {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, OnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Ingreso exitoso!", Toast.LENGTH_LONG).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this, "Ingreso falló :(", Toast.LENGTH_LONG).show()
+                        }
+                    })
+            }
         }
+
+
     }
 }
